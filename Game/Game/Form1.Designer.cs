27adc99f53ac0c41186d7fbed54338113198b18a
AcLior <44374208+AcLior@users.noreply.github.com>
2021-05-08 11:12:48 +0300

    GameVer2

--- Game/Game/Form1.Designer.cs
+++ Game/Game/Form1.Designer.cs
@@ -43,11 +43,27 @@ namespace Game
             this.birdPlayer = new System.Windows.Forms.PictureBox();
             this.tmrMoveBird = new System.Windows.Forms.Timer(this.components);
             this.lblScore = new System.Windows.Forms.Label();
+            this.lblLimit = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.lblYourScore = new System.Windows.Forms.Label();
+            this.picTree1 = new System.Windows.Forms.PictureBox();
+            this.picTree2 = new System.Windows.Forms.PictureBox();
+            this.picShrub1 = new System.Windows.Forms.PictureBox();
+            this.picTree3 = new System.Windows.Forms.PictureBox();
+            this.picShrub2 = new System.Windows.Forms.PictureBox();
+            this.picShrub3 = new System.Windows.Forms.PictureBox();
+            this.tmrPlant = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.rock1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rock2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rock3)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.birdPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShrub1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShrub2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShrub3)).BeginInit();
             this.SuspendLayout();
             // 
             // tmrMove1
@@ -151,18 +167,129 @@ namespace Game
             this.lblScore.Size = new System.Drawing.Size(0, 31);
             this.lblScore.TabIndex = 8;
             // 
+            // lblLimit
+            // 
+            this.lblLimit.AutoSize = true;
+            this.lblLimit.Location = new System.Drawing.Point(12, 345);
+            this.lblLimit.Name = "lblLimit";
+            this.lblLimit.Size = new System.Drawing.Size(0, 13);
+            this.lblLimit.TabIndex = 9;
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.lblGameOver.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblGameOver.Location = new System.Drawing.Point(200, 116);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(387, 76);
+            this.lblGameOver.TabIndex = 10;
+            this.lblGameOver.Text = "Game Over";
+            this.lblGameOver.Visible = false;
+            // 
+            // lblYourScore
+            // 
+            this.lblYourScore.AutoSize = true;
+            this.lblYourScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourScore.Location = new System.Drawing.Point(251, 220);
+            this.lblYourScore.Name = "lblYourScore";
+            this.lblYourScore.Size = new System.Drawing.Size(129, 25);
+            this.lblYourScore.TabIndex = 11;
+            this.lblYourScore.Text = "Your score: ";
+            this.lblYourScore.Visible = false;
+            // 
+            // picTree1
+            // 
+            this.picTree1.Image = ((System.Drawing.Image)(resources.GetObject("picTree1.Image")));
+            this.picTree1.Location = new System.Drawing.Point(128, 273);
+            this.picTree1.Name = "picTree1";
+            this.picTree1.Size = new System.Drawing.Size(112, 107);
+            this.picTree1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picTree1.TabIndex = 12;
+            this.picTree1.TabStop = false;
+            this.picTree1.Visible = false;
+            // 
+            // picTree2
+            // 
+            this.picTree2.Image = ((System.Drawing.Image)(resources.GetObject("picTree2.Image")));
+            this.picTree2.Location = new System.Drawing.Point(387, 353);
+            this.picTree2.Name = "picTree2";
+            this.picTree2.Size = new System.Drawing.Size(93, 93);
+            this.picTree2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picTree2.TabIndex = 13;
+            this.picTree2.TabStop = false;
+            this.picTree2.Visible = false;
+            // 
+            // picShrub1
+            // 
+            this.picShrub1.Image = ((System.Drawing.Image)(resources.GetObject("picShrub1.Image")));
+            this.picShrub1.Location = new System.Drawing.Point(148, 422);
+            this.picShrub1.Name = "picShrub1";
+            this.picShrub1.Size = new System.Drawing.Size(92, 24);
+            this.picShrub1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShrub1.TabIndex = 14;
+            this.picShrub1.TabStop = false;
+            this.picShrub1.Visible = false;
+            // 
+            // picTree3
+            // 
+            this.picTree3.Image = ((System.Drawing.Image)(resources.GetObject("picTree3.Image")));
+            this.picTree3.Location = new System.Drawing.Point(692, 345);
+            this.picTree3.Name = "picTree3";
+            this.picTree3.Size = new System.Drawing.Size(96, 93);
+            this.picTree3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picTree3.TabIndex = 15;
+            this.picTree3.TabStop = false;
+            this.picTree3.Visible = false;
+            // 
+            // picShrub2
+            // 
+            this.picShrub2.Image = ((System.Drawing.Image)(resources.GetObject("picShrub2.Image")));
+            this.picShrub2.Location = new System.Drawing.Point(273, 411);
+            this.picShrub2.Name = "picShrub2";
+            this.picShrub2.Size = new System.Drawing.Size(92, 24);
+            this.picShrub2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShrub2.TabIndex = 16;
+            this.picShrub2.TabStop = false;
+            this.picShrub2.Visible = false;
+            // 
+            // picShrub3
+            // 
+            this.picShrub3.Image = ((System.Drawing.Image)(resources.GetObject("picShrub3.Image")));
+            this.picShrub3.Location = new System.Drawing.Point(506, 422);
+            this.picShrub3.Name = "picShrub3";
+            this.picShrub3.Size = new System.Drawing.Size(92, 24);
+            this.picShrub3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShrub3.TabIndex = 17;
+            this.picShrub3.TabStop = false;
+            this.picShrub3.Visible = false;
+            // 
+            // tmrPlant
+            // 
+            this.tmrPlant.Interval = 200;
+            this.tmrPlant.Tick += new System.EventHandler(this.tmrPlant_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblYourScore);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblLimit);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.birdPlayer);
             this.Controls.Add(this.rock3);
             this.Controls.Add(this.rock2);
             this.Controls.Add(this.rock1);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.picTree1);
+            this.Controls.Add(this.picShrub1);
+            this.Controls.Add(this.picShrub2);
+            this.Controls.Add(this.picTree2);
+            this.Controls.Add(this.picShrub3);
+            this.Controls.Add(this.picTree3);
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -174,6 +301,12 @@ namespace Game
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.birdPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShrub1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShrub2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShrub3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,6 +326,16 @@ namespace Game
         private System.Windows.Forms.PictureBox birdPlayer;
         private System.Windows.Forms.Timer tmrMoveBird;
         private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblLimit;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.Label lblYourScore;
+        private System.Windows.Forms.PictureBox picTree1;
+        private System.Windows.Forms.PictureBox picTree2;
+        private System.Windows.Forms.PictureBox picShrub1;
+        private System.Windows.Forms.PictureBox picTree3;
+        private System.Windows.Forms.PictureBox picShrub2;
+        private System.Windows.Forms.PictureBox picShrub3;
+        private System.Windows.Forms.Timer tmrPlant;
     }
 }
 
--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -13,13 +13,12 @@ namespace Game
     public partial class Form1 : Form
     {
         int counter = 0;
-        
-        
+
+
         public Form1()
         {
-           
-            InitializeComponent();
 
+            InitializeComponent();
         }
 
         private void MoveRock1(int speed)
@@ -30,10 +29,12 @@ namespace Game
                 rock1.Left += -speed;
 
             }
-            else {
+            else
+            {
                 tmrMove2.Enabled = true;
                 rock2.Visible = true;
-                rock1.Left = 1020; }
+                rock1.Left = 1020;
+            }
         }
         private void MoveRock2(int speed)
         {
@@ -42,10 +43,12 @@ namespace Game
                 rock2.Left += -speed;
 
             }
-            else {
+            else
+            {
                 tmrMove3.Enabled = true;
                 rock3.Visible = true;
-                rock2.Left = 1020; }
+                rock2.Left = 1020;
+            }
         }
         private void MoveRock3(int speed)
         {
@@ -63,6 +66,7 @@ namespace Game
 
             MoveRock1(15);
 
+
         }
 
         private void tmrMove3_Tick(object sender, EventArgs e)
@@ -77,15 +81,23 @@ namespace Game
 
         private void startGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
-            rock1.Location= new Point(762,422);
-            rock2.Location= new Point(762, 422);
-            rock3.Location= new Point(762, 422);
+            lblScore.Location = new Point(728, 60);
+            lblYourScore.Visible = false;
+            lblGameOver.Visible = false;
+            rock1.Location = new Point(762, 422);
+            rock2.Location = new Point(762, 422);
+            rock3.Location = new Point(762, 422);
 
             rock1.Visible = true;
             birdPlayer.Visible = true;
+            picTree1.Visible = true;
+            picTree2.Visible = true;
+            picTree3.Visible = true;
+            picShrub1.Visible = true;
+            picShrub2.Visible = true;
+            picShrub3.Visible = true;
             tmrMove1.Start();
-
+            tmrPlant.Start();
 
 
         }
@@ -99,6 +111,11 @@ namespace Game
             tmrMove1.Stop();
             tmrMove2.Stop();
             tmrMove3.Stop();
+            tmrPlant.Stop();
+            lblScore.Location = new Point(430, 218);
+            lblYourScore.Visible = true;
+            lblGameOver.Visible = true;
+
             counter = 0;
         }
         private void endGameToolStripMenuItem_Click(object sender, EventArgs e)
@@ -110,7 +127,7 @@ namespace Game
         {
             if (birdPlayer.Location.Y <= 399)
             {
-                birdPlayer.Top+=2;
+                birdPlayer.Top += 10;
 
             }
 
@@ -120,24 +137,105 @@ namespace Game
 
         private void CheckError()
         {
-            if (birdPlayer.Bounds.IntersectsWith(rock1.Bounds)|| birdPlayer.Bounds.IntersectsWith(rock2.Bounds)  || birdPlayer.Bounds.IntersectsWith(rock3.Bounds))
+            if (birdPlayer.Bounds.IntersectsWith(rock1.Bounds) || birdPlayer.Bounds.IntersectsWith(rock2.Bounds) || birdPlayer.Bounds.IntersectsWith(rock3.Bounds))
             {
                 GameOver();
             }
-           
+
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-           
-            if (e.KeyCode==Keys.Space)
-            {
-                
-                birdPlayer.Top -= 40;
-            }
-          
-        }
 
-       
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!birdPlayer.Bounds.IntersectsWith(lblLimit.Bounds))
+                {
+                    birdPlayer.Top -= 30;
+
+                }
+
+            }
+
+        }
+        private void tmrPlant_Tick(object sender, EventArgs e)
+        {
+            picTree1.Left -= 10;
+            picTree2.Left -= 10;
+            picTree3.Left -= 10;
+            picShrub1.Left -= 10;
+            picShrub2.Left -= 10;
+            picShrub3.Left -= 10;
+
+            if (picTree1.Left >= 0)
+            {
+                picTree1.Left += -10;
+
+            }
+            else
+            {
+                picTree1.Visible = true;
+                picTree1.Left = 1020;
+            }
+
+            if (picTree2.Left >= 0)
+            {
+                picTree2.Left += -10;
+
+            }
+            else
+            {
+                picTree2.Visible = true;
+                picTree2.Left = 1020;
+            }
+
+            if (picTree3.Left >= 0)
+            {
+                picTree3.Left += -10;
+
+            }
+            else
+            {
+                picTree3.Visible = true;
+                picTree3.Left = 1020;
+            }
+
+
+
+            if (picShrub1.Left >= 0)
+            {
+                picShrub1.Left += -10;
+
+            }
+            else
+            {
+                picShrub1.Visible = true;
+                picShrub1.Left = 1020;
+            }
+
+
+            if (picShrub2.Left >= 0)
+            {
+                picShrub2.Left += -10;
+
+            }
+            else
+            {
+                picShrub2.Visible = true;
+                picShrub2.Left = 1020;
+            }
+
+
+            if (picShrub3.Left >= 0)
+            {
+                picShrub3.Left += -10;
+
+            }
+            else
+            {
+                picShrub3.Visible = true;
+                picShrub3.Left = 1020;
+            }
+        }
     }
 }
